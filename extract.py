c17c2bdff814a3d3581aa9d6fe8d0f8d687fe392
fabpiaf <144336756+fabpiaf@users.noreply.github.com>
2025-04-24 16:21:09 +0000

    further improvements: use pathlib.path, use shebang, iterate over all cli args, fallback to requested path if not filename is found in response

--- extract.py
+++ extract.py
@@ -1,52 +1,58 @@
+#!/bin/python
 import base64
 import re
 import xml.etree.ElementTree as ET
-
+import sys
 # Variables
-path = 'document-download.burp'
-output_path = './extracted/'
+#path = 'document-download.burp'
+from pathlib import Path
+output_path = Path()
 
 def main():
     # Parse Burp file
-    mytree = ET.parse(path)
-    myroot = mytree.getroot()
+    for path in sys.argv[1:]:
+        mytree = ET.parse(path)
+        myroot = mytree.getroot()
 
-    # Search through each item in the file
-    for item in myroot.findall('item'):
-        try:
-            # Retreive the response
-            based = (item.find('response').text)
+        # Search through each item in the file
+        for item in myroot.findall('item'):
+            try:
+                # Retreive the response
+                based = (item.find('response').text)
+                req = (item.find('request').text)
 
-            # Decode the response
-            data = base64.b64decode(based)
+                # Decode the response
+                data = base64.b64decode(based)
+                httpverb, reqpath, rest = base64.b64decode(req).decode('utf-8').split(' ',2)
 
-            # Retrieve the headers from the response
-            headers = data.split(b'\r\n\r\n')[0]
 
-            # Retrieve the content from the response
-            content = data.split(b'\r\n\r\n')[1]
 
-            # Extract the filename from the response headers
-            regex_expression = r'filename=(?:\")?([a-zA-Z0-9\-\_\.\_]*)(?:\")?'
-            filenames = re.findall(regex_expression, headers.decode("utf-8"))
-            if len(filenames) <= 0 :
-                raise Exception("No filename was identified")
+                # Retrieve the headers from the response
+                headers = data.split(b'\r\n\r\n')[0]
 
-            # Get the first match
-            filename = filenames[0]
+                # Retrieve the content from the response
+                content = data.split(b'\r\n\r\n')[1]
 
-            # Generate the output path
-            output_name = output_path + filename
-            print (f"[+] Extracted : {filename}")
+                # Extract the filename from the response headers
+                regex_expression = r'filename=(?:\")?([a-zA-Z0-9\-\_\.\_]*)(?:\")?'
+                filenames = re.findall(regex_expression, headers.decode("utf-8"))
+                if len(filenames) <= 0 :
+                    filename=reqpath.split('/')[-1]
+                    print(f'[+] not filename in response detected, falling back to requested: {filename}')
+                else:
+                    # Get the first match
+                    filename = filenames[0]
 
-            # Write the body to a file using extracted filename
-            f = open(output_name, "wb")
-            f.write(content)
-            f.close()
+                # Generate the output path
 
-        # If something goes wrong, print the exception
-        except Exception as e:
-            print(e)
+                with (output_path/filename).open("wb") as f:
+                    # Write the body to a file using extracted filename
+                    f.write(content)
+                print (f"[+] Extracted : {filename}")
+
+            # If something goes wrong, print the exception
+            except Exception as e:
+                print(e)
 
 if __name__ == "__main__":
     main()
